9271f7a8a0f75995a3acc84d5132c2c23afb4e28
Jaco Greeff <jaco@ethcore.io>
2016-08-05 13:09:09 +0200

    remove components import (for now)

--- src/components/util/account.js
+++
@@ -1,9 +0,0 @@
-import { BASE_LINK_ACCOUNT_MORDEN, BASE_LINK_ACCOUNT_HOMESTEAD } from '../constants/constants';
-
-export const getAccountLink = _getAccountLink;
-
-function _getAccountLink (address, chain) {
-  const isTestNet = chain === 'morden' || chain === 'testnet';
-  const base = isTestNet ? BASE_LINK_ACCOUNT_MORDEN : BASE_LINK_ACCOUNT_HOMESTEAD;
-  return base + address;
-}
--- src/components/util/react.js
+++
@@ -1,6 +0,0 @@
-
-import { isValidElement } from 'react';
-
-export function isReactComponent (componentOrElem) {
-  return isValidElement(componentOrElem) && typeof componentOrElem.type === 'function';
-}
