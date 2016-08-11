817175d955ccdf45258ef561eeb0dc94dde93965
Jaco Greeff <jaco@ethcore.io>
2016-08-11 09:35:49 +0200

    polyfills

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import { Redirect, Router, Route, useRouterHistory } from 'react-router';
-import { createHashHistory } from 'history';
+import 'isomorphic-fetch';
+
+import es6Promise from 'es6-promise';
+es6Promise.polyfill();
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
+
+import { Redirect, Router, Route, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 
 import Accounts from './components/Accounts';
 import Account from './components/Account';
